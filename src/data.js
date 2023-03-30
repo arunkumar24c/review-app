a389de4fb6ac597fc1f534ee5eb9815830c5a87f
arunkumar24c <arunkumar24c@gmail.com>
2023-03-30 18:58:19 +0530

    update

---
+++ src/data.js
@@ -0,0 +1,44 @@
+
+
+
+
+const people = [
+    {
+        id: 1,
+        image:
+           "https://images.unsplash.com/photo-1540569014015-19a7be504e3a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+        name: 'naveen',
+        title: 'office manager',
+        quote:
+            'Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.',
+    },
+    {
+        id: 2,
+        image:
+            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+        name: 'arun',
+        title: 'regular guy',
+        quote:
+            'Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.',
+    },
+    {
+        id: 3,
+        image:
+            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg',
+        name: 'Dhin',
+        title: 'product designer',
+        quote:
+            'Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.',
+    },
+    {
+        id: 4,
+        image:
+            "https://images.unsplash.com/photo-1544723795-3fb6469f5b39?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=389&q=80",
+        name: 'praveen',
+        title: 'Project Manager',
+        quote:
+            'Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ',
+    },
+];
+
+export default people;
